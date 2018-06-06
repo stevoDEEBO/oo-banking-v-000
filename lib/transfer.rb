db63079eb6471818091330d1c349315f848e057e
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-06 18:58:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       @status = "complete"
     else
       @status = "rejected"
-      "Transaction rejected. Please check your account balance."
+      puts "Transaction rejected. Please check your account balance."
     end
   end
 
