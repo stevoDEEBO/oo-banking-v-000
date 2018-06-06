14cd571937f289d50c31e07965f85135c4ece5d4
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-06 18:50:30 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance >= @amount
+    if @status == "pending" && @sender.balance >= @amount
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
