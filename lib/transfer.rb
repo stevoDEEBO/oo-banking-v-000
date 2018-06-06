8577d8162f9a6207dde779ebe6aadf74c2c7c4e8
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-06 18:04:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
 
 
-  def initialize(sender, receiver, status, amount)
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
