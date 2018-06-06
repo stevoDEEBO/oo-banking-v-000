b6358f7351d23c29a735895946791ee7abc739e3
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-06 18:49:45 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,6 +29,9 @@ class Transfer
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
+    else
+      @sender.balance = @sender.balance
+      @receiver.balance = @receiver.balance
     end
   end
 end
